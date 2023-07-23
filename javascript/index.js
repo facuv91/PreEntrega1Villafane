2eb87aa0f19aecf0cee3cad1cc045a9b3becc65f
Facundo <facu.v.91@gmail.com>
2023-07-23 18:31:34 -0300

    Aplicacion de funciones

--- javascript/index.js
+++ javascript/index.js
@@ -5,53 +5,85 @@ let monto = parseInt(prompt("Ingrese el monto del préstamo a solicitar:"))
 let cuotas = parseInt(prompt("ingrese el la cantidad de cuotas en que desea pagarlo:"))
 let eleccion = prompt("¿Desea calcular la afectacion de sus ingresos?  Responda SI o NO")
 
+function calculoCoutaCapital (mon,cuo){
+    capital = mon/cuo;
+} 
+
+function calculoInteres (mon){
+    switch (destino){
+        case "UNO":{
+            let tasa=80;
+            interes = (((mon/100)*tasa)/365)*30;
+        }
+        case "DOS":{
+            let tasa=70;
+            interes = (((mon/100)*tasa)/365)*30;
+        }
+        case "TRES":{
+            let tasa=18;
+            interes = (((mon/100)*tasa)/365)*30;
+        }
+        break;
+    }
+}
+
+function calculoIva(int){
+    iva = int*0.21;
+}
+
+function calculoCuotaMensual(cap,int,iva){
+    cuotaMensual=cap+int+iva;
+}
+
+function acuerdo (mon,cap,int,iva,ctaMen,cta){
+    console.log("Acordado" +" "+"$"+mon);
+    switch (destino){
+        case "UNO":{
+            let tasa=80;
+            console.log("TNA "+tasa+"%");
+        }
+        break;
+        case "DOS":{
+            let tasa=70;
+            console.log("TNA "+tasa+"%");
+        }
+        break;
+        case "TRES":{
+            let tasa=18;
+            console.log("TNA "+tasa+"%");
+        }
+        break;
+    }    
+    console.log("Capital a abonar"+" "+"$"+cap);
+    console.log("Interes a abonar por cuota"+" "+"$"+int);
+    console.log("Iva="+" "+"$"+iva);
+    console.log("Cuota total mensual"+" "+"$"+ctaMen);
+    console.log("A pagar en"+" "+cta+" "+"cuotas mensuales, iguales y consecutivas");    
+}
+
 switch (destino){
     case"UNO": if (monto<=2000000 && cuotas<=60){
-        let tasa = 80;
-        let capital = (monto/cuotas);    
-        let interes = (((monto/100)*tasa)/365)*30;
-        let iva = interes*0.21;    
-        let cuotaMensual = capital + interes + iva;
-    
-        console.log("Acordado" +" "+"$"+monto);
-        console.log("TNA 80%");
-        console.log("Capital a abonar"+" "+"$"+capital);
-        console.log("Interes a abonar por cuota"+" "+"$"+interes);
-        console.log("Iva="+" "+"$"+iva);
-        console.log("Cuota total mensual"+" "+"$"+cuotaMensual);
-        console.log("A pagar en"+" "+cuotas+" "+"cuotas mensuales, iguales y consecutivas");
+        calculoCoutaCapital (monto,cuotas);
+        calculoInteres(monto);   
+        calculoIva(interes);
+        calculoCuotaMensual(capital, interes, iva);
+        acuerdo (monto,capital,interes,iva,cuotaMensual,cuotas);
     }
     break;
     case "DOS": if (monto<=7000000 && cuotas<=24){
-        let tasa=70;
-        let capital = (monto/cuotas);    
-        let interes = (((monto/100)*tasa)/365)*30;
-        let iva = interes*0.21;    
-        let cuotaMensual = capital + interes + iva;
-    
-        console.log("Acordado" +" "+"$"+monto);
-        console.log("TNA 70%");
-        console.log("Capital a abonar"+" "+"$"+capital);
-        console.log("Interes a abonar por cuota"+" "+"$"+interes);
-        console.log("Iva="+" "+"$"+iva);
-        console.log("Cuota total mensual"+" "+"$"+cuotaMensual);
-        console.log("A pagar en"+" "+cuotas+" "+"cuotas mensuales, iguales y consecutivas");
+        calculoCoutaCapital (monto,cuotas);
+        calculoInteres(monto);   
+        calculoIva(interes);
+        calculoCuotaMensual(capital, interes, iva);
+        acuerdo (monto,capital,interes,iva,cuotaMensual,cuotas);
     }
     break;
     case"TRES": if (monto<=30000000 && cuotas>=120 && cuotas<=360){
-        let tasa=18;
-        let capital = (monto/cuotas);    
-        let interes = (((monto/100)*tasa)/365)*30;
-        let iva = interes*0.21;    
-        let cuotaMensual = capital + interes + iva;
-    
-        console.log("Acordado" +" "+"$"+monto);
-        console.log("TNA 18%");
-        console.log("Capital a abonar"+" "+"$"+capital);
-        console.log("Interes a abonar por cuota"+" "+"$"+interes);
-        console.log("Iva="+" "+"$"+iva);
-        console.log("Cuota total mensual"+" "+"$"+cuotaMensual);
-        console.log("A pagar en"+" "+cuotas+" "+"cuotas mensuales, iguales y consecutivas");
+        calculoCoutaCapital (monto,cuotas);
+        calculoInteres(monto);   
+        calculoIva(interes);
+        calculoCuotaMensual(capital, interes, iva);
+        acuerdo (monto,capital,interes,iva,cuotaMensual,cuotas);
     }
     break;
     default:
@@ -71,73 +103,73 @@ switch (eleccion){
                     case "NO":
                     let independiente = prompt("Trabaja de manera independiente? SI O NO");
                     switch (independiente){
-                        case "SI":
+                        case "SI":{
                             let afip = prompt("En caso de ser responsable inscripto escriba SI,  de lo contrario escriba NO");
                             switch (afip){
-                                case "SI":
+                                case "SI":{
                                     let liquidacionAnual = parseInt(prompt("Ingrese el monto de su liquidacion anual"));
                                     let afectacion = ((liquidacionAnual/12)/100)*35;
                                     console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion);                
-                                break;
-                                case "NO":
+                                }break;
+                                case "NO":{
                                 let monotributista = prompt("Escriba su categoria de Monotributista");
                                     switch (monotributista){
-                                        case "A":
+                                        case "A":{
                                             let afectacion = (((1400000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion);
-                                        break;
-                                        case "B":
-                                            let afectacion1 = (((2100000/12))/100)*35;
-                                            console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion1);
-                                        break;
-                                        case "C":
+                                        }break;
+                                        case "B":{
+                                            let afectacion = (((2100000/12))/100)*35;
+                                            console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion);
+                                        }break;
+                                        case "C":{
                                             let afectacion2 = (((2900000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion2);
-                                        break;
-                                        case "D":
+                                        }break;
+                                        case "D":{
                                             let afectacion3 = (((3600000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion3);
-                                        break;
-                                        case "E":
+                                        }break;
+                                        case "E":{
                                             let afectacion4 = (((4300000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion4);
-                                        break;
-                                        case "F":
+                                        }break;
+                                        case "F":{
                                             let afectacion5 = (((5300000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion5);
-                                        break;
-                                        case "G":
+                                        }break;
+                                        case "G":{
                                             let afectacion6 = (((6400000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion6);
-                                        break;
-                                        case "H":
+                                        }break;
+                                        case "H":{
                                             let afectacion7 = (((7900000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion7); 
-                                        break;
-                                        case "I":
+                                        }break;
+                                        case "I":{
                                             let afectacion8 = (((8900000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion8); 
-                                        break;
-                                        case "J":
+                                        }break;
+                                        case "J":{
                                             let afectacion9 = (((10200000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion9);
-                                        break;
-                                        case "K":
+                                        }break;
+                                        case "K":{
                                             let afectacion10 = (((11300000/12))/100)*35;
                                             console.log("La cuota que usted puede abonar es de"+" "+"$"+afectacion10); 
-                                        break;
+                                        }break;
                                     }
-                        break;            
-                        case "NO":            
+                                }break;            
+                        case "NO":{            
                             alert("usted no posee ingresos que puedan ser analizados, muchas gracias por su visita");
-                            break;
+                        }break;
                         }
                     }
                 }
-    break;
-    case "NO": 
+            }break;
+    case "NO":{ 
             alert("Muchas gracias por su visita");
-            break;
+    }break;
 }
 
 
